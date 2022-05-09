720e0f48a88aa31f4549be062136168fdea79cbe
oishigyunyu <yamamuuuura@gmail.com>
2022-05-09 22:01:31 +0900

    part2

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,34 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello world!',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(32),
+                child: Image.network(
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                  height: 100,
+                  width: 100,
+                ),
+              ),
+              Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  )),
+            ],
+          ),
+          const Text(
+            'pikachu',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          const Chip(
+            label: Text('electric'),
+            backgroundColor: Colors.yellow,
+          )
+        ]),
       ),
     );
   }
