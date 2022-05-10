97ea720e9caf28af1b34bbacccb91d00eaac50aa
oishigyunyu <yamamuuuura@gmail.com>
2022-05-10 22:51:01 +0900

    ボタンを１万個つくる

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import './poke_detail.dart';
 
@@ -13,18 +15,27 @@ class TopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('detail'),
-          onPressed: () => {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => const PokeDetail(),
-              ),
-            ),
-          },
+      body: ListView.builder(
+          itemCount: 10000,
+          itemBuilder: (context, index) => const PikaButton()),
+    );
+  }
+}
+
+class PikaButton extends StatelessWidget {
+  const PikaButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: const Text('detail'),
+      onPressed: () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => const PokeDetail(),
+          ),
         ),
-      ),
+      },
     );
   }
 }
