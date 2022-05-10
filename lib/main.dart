9d66035357aeba13b0ac2dd8aa4a08a32b93e92f
oishigyunyu <yamamuuuura@gmail.com>
2022-05-10 23:10:55 +0900

    setp3まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import './poke_detail.dart';
 
@@ -17,23 +15,40 @@ class TopPage extends StatelessWidget {
     return Scaffold(
       body: ListView.builder(
           itemCount: 10000,
-          itemBuilder: (context, index) => const PikaButton()),
+          itemBuilder: (context, index) => PokeListItem(index: index)),
     );
   }
 }
 
-class PikaButton extends StatelessWidget {
-  const PikaButton({Key? key}) : super(key: key);
-
+class PokeListItem extends StatelessWidget {
+  const PokeListItem({Key? key, required this.index}) : super(key: key);
+  final int index;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      child: const Text('detail'),
-      onPressed: () => {
+    return ListTile(
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+            color: Colors.yellow.withOpacity(.5),
+            borderRadius: BorderRadius.circular(10),
+            image: const DecorationImage(
+              fit: BoxFit.fitWidth,
+              image: NetworkImage(
+                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
+            )),
+      ),
+      title: const Text(
+        'Pikachu',
+        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text(
+        '⚡️electric',
+      ),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
         Navigator.of(context).push(
           MaterialPageRoute(
-            builder: (BuildContext context) => const PokeDetail(),
-          ),
+              builder: (BuildContext context) => const PokeDetail()),
         ),
       },
     );
