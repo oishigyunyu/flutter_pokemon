fc4b8df14398d5bf46a03e4e8176b8cb2493e9f2
oishigyunyu <yamamuuuura@gmail.com>
2022-05-12 22:24:46 +0900

    poke_list_itemの分離

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
-import './poke_detail.dart';
+import './poke_list_item.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: TopPage(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokemon Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
 }
 
 class TopPage extends StatelessWidget {
@@ -13,44 +25,12 @@ class TopPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10000,
-          itemBuilder: (context, index) => PokeListItem(index: index)),
-    );
-  }
-}
-
-class PokeListItem extends StatelessWidget {
-  const PokeListItem({Key? key, required this.index}) : super(key: key);
-  final int index;
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Container(
-        width: 80,
-        decoration: BoxDecoration(
-            color: Colors.yellow.withOpacity(.5),
-            borderRadius: BorderRadius.circular(10),
-            image: const DecorationImage(
-              fit: BoxFit.fitWidth,
-              image: NetworkImage(
-                  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
-            )),
-      ),
-      title: const Text(
-        'Pikachu',
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-      ),
-      subtitle: const Text(
-        '⚡️electric',
-      ),
-      trailing: const Icon(Icons.navigate_next),
-      onTap: () => {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-              builder: (BuildContext context) => const PokeDetail()),
-        ),
-      },
+      body: SafeArea(
+          child: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+        itemCount: 898,
+        itemBuilder: (context, index) => PokeListItem(index: index),
+      )),
     );
   }
 }
